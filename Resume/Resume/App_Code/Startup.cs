70aa01a8b3160e45db81b4b080fcf0eed6fc3c71
Jared Hawkins <jaredhawkins401@gmail.com>
2019-07-03 17:43:43 -0400

    Base Project Added, No Edits

---
+++ Resume/Resume/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Resume.Startup))]
+namespace Resume
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
